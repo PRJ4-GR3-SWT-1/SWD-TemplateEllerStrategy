abdbadc612f64ed939ebc9f425e7b75262951bb4
AnnSo <64138186+auAnnSo@users.noreply.github.com>
2021-03-16 13:34:10 +0100

    bubblesort done

--- SWD-TemplateEllerGof/BubbleSort.cs
+++ SWD-TemplateEllerGof/BubbleSort.cs
@@ -4,7 +4,25 @@ using System.Text;
 
 namespace SWD_TemplateEllerGof
 {
-    public class BubbleSort
+    public class BubbleSort: ISorter
     {
+        public int[] ConcreteSorter(int[] unsortedArray)
+        {
+            int t;
+            for (int p = 0; p <= unsortedArray.Length - 2; p++)
+            {
+                for (int i = 0; i <= unsortedArray.Length - 2; i++)
+                {
+                    if (unsortedArray[i] > unsortedArray[i + 1])
+                    {
+                        t = unsortedArray[i + 1];
+                        unsortedArray[i + 1] = unsortedArray[i];
+                        unsortedArray[i] = t;
+                    }
+                }
+            }
+
+            return unsortedArray;
+        }
     }
 }
